dac1e9d8142c1d44093735a150763008ea41dd9d
Neil Pathare <pathare@synopsys.com>
2022-01-14 16:14:05 -0500

    Adding next test token

--- src/main/java/com/scalesec/vulnado/auth/Auth.java
+++ src/main/java/com/scalesec/vulnado/auth/Auth.java
@@ -1,6 +1,7 @@
 package com.scalesec.vulnado.auth;
 
 public class Auth {
-  public String publicTOken = "abc";
+  public String publicToken1 = "abc";
+  public String publicToken2 = "abc";
   private String token = "abc";
 }
