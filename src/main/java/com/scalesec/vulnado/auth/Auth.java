f7450c5380e802eb36b9439cd2c348c12bf480a5
Rahul Gunasekaran <71350204+rahulguna10@users.noreply.github.com>
2022-03-23 10:35:49 -0400

    Update Auth.java

--- src/main/java/com/scalesec/vulnado/auth/Auth.java
+++ src/main/java/com/scalesec/vulnado/auth/Auth.java
@@ -6,6 +6,7 @@ public class Auth {
   public String publicToken2 = "abc";
   private String secrettoken = "H879h9hfh9ehf0wf";
   private String password = "password";
+  private String jwt_token = "ey09sdh38r3r39r3r93rj39rj3r9hr39rh";
   private String username = "admin";
   
 }
